9ce955e5821690fc37a52a8ec1e2b6f170384d49
GerryHorror <GerryHorror@users.noreply.github.com>
2024-10-12 22:40:12 +0200

    Added Entity Framework Core packages to CMCS.csproj for SQL Server support and tooling. Created CMCSDbContext class in CMCS.Data namespace with DbSet properties for UserModel, ClaimModel, DocumentModel, RoleModel, and ClaimStatus to manage these entities.

---
+++ Data/CMCSDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CMCS.Models;
+
+namespace CMCS.Data
+{
+    public class CMCSDbContext : DbContext
+    {
+        public CMCSDbContext(DbContextOptions<CMCSDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<ClaimModel> Claims { get; set; }
+        public DbSet<DocumentModel> Documents { get; set; }
+        public DbSet<RoleModel> Roles { get; set; }
+        public DbSet<ClaimStatus> ClaimStatuses { get; set; }
+    }
+}
